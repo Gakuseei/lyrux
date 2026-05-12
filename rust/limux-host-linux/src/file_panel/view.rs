d758c3f08d247f90aad921d47aa3cd25841c7312
Gakuseei <erikschaefer07@icloud.com>
2026-05-12 22:57:43 +0200

    feat(file_panel): ListView and signal-list-item-factory

--- rust/limux-host-linux/src/file_panel/view.rs
+++ rust/limux-host-linux/src/file_panel/view.rs
@@ -4,6 +4,9 @@ use std::rc::Rc;
 use gtk4 as gtk;
 use gtk4::prelude::*;
 
+use crate::file_panel::model::{GitStatus, Kind, TreeModel};
+use crate::file_panel::row_object::RowObject;
+
 #[allow(dead_code)]
 pub struct HeaderHandle {
     pub root: gtk::Box,
@@ -68,4 +71,134 @@ pub struct ViewState {
 #[allow(dead_code)]
 pub fn placeholder_state() -> Rc<RefCell<Option<ViewState>>> {
     Rc::new(RefCell::new(None))
+}
+
+#[allow(dead_code)]
+pub fn build_list_view() -> ViewState {
+    let store = gtk4::gio::ListStore::new::<RowObject>();
+    let selection = gtk::MultiSelection::new(Some(store.clone()));
+    let factory = gtk::SignalListItemFactory::new();
+    factory.connect_setup(|_, item| {
+        let item = item.downcast_ref::<gtk::ListItem>().unwrap();
+        let row_box = gtk::Box::new(gtk::Orientation::Horizontal, 4);
+        row_box.add_css_class("limux-fp-row");
+        let indent = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+        indent.set_width_request(0);
+        indent.add_css_class("limux-fp-indent");
+        let chevron = gtk::Image::from_icon_name("pan-end-symbolic");
+        chevron.add_css_class("limux-fp-chevron");
+        let icon = gtk::Image::from_icon_name("text-x-generic-symbolic");
+        icon.add_css_class("limux-fp-rowicon");
+        let label = gtk::Label::new(None);
+        label.set_xalign(0.0);
+        label.set_hexpand(true);
+        label.add_css_class("limux-fp-name");
+        let marker = gtk::Label::new(None);
+        marker.add_css_class("limux-fp-git");
+        row_box.append(&indent);
+        row_box.append(&chevron);
+        row_box.append(&icon);
+        row_box.append(&label);
+        row_box.append(&marker);
+        item.set_child(Some(&row_box));
+    });
+    factory.connect_bind(|_, item| {
+        let item = item.downcast_ref::<gtk::ListItem>().unwrap();
+        let row_obj = match item.item().and_then(|o| o.downcast::<RowObject>().ok()) {
+            Some(o) => o,
+            None => return,
+        };
+        let row_box = match item.child().and_then(|w| w.downcast::<gtk::Box>().ok()) {
+            Some(b) => b,
+            None => return,
+        };
+        bind_row(&row_box, &row_obj);
+    });
+    let list_view = gtk::ListView::new(Some(selection.clone()), Some(factory));
+    list_view.add_css_class("limux-fp-listview");
+    list_view.set_show_separators(false);
+    ViewState {
+        store,
+        selection,
+        list_view,
+    }
+}
+
+#[allow(dead_code)]
+fn bind_row(row_box: &gtk::Box, row_obj: &RowObject) {
+    let mut children: Vec<gtk::Widget> = Vec::new();
+    let mut child = row_box.first_child();
+    while let Some(w) = child {
+        child = w.next_sibling();
+        children.push(w);
+    }
+    if children.len() != 5 {
+        return;
+    }
+    let indent = children[0].clone();
+    let chevron = children[1].clone().downcast::<gtk::Image>().unwrap();
+    let icon = children[2].clone().downcast::<gtk::Image>().unwrap();
+    let label = children[3].clone().downcast::<gtk::Label>().unwrap();
+    let marker = children[4].clone().downcast::<gtk::Label>().unwrap();
+
+    let depth = row_obj.depth();
+    indent.set_width_request((depth as i32) * 16);
+    for c in 0..=8 {
+        let class = format!("limux-fp-depth-{c}");
+        row_box.remove_css_class(&class);
+    }
+    let class = format!("limux-fp-depth-{}", depth.min(8));
+    row_box.add_css_class(&class);
+
+    let is_dir = matches!(row_obj.kind(), Kind::Dir);
+    if is_dir {
+        chevron.set_visible(true);
+        chevron.set_icon_name(Some(if row_obj.expanded() {
+            "pan-down-symbolic"
+        } else {
+            "pan-end-symbolic"
+        }));
+        icon.set_icon_name(Some("folder-symbolic"));
+    } else {
+        chevron.set_visible(false);
+        icon.set_icon_name(Some("text-x-generic-symbolic"));
+    }
+
+    label.set_text(&row_obj.name());
+
+    let (text, css) = git_marker_for(row_obj.git_status());
+    marker.set_text(text);
+    for c in [
+        "limux-fp-git-m",
+        "limux-fp-git-a",
+        "limux-fp-git-d",
+        "limux-fp-git-u",
+        "limux-fp-git-c",
+    ] {
+        marker.remove_css_class(c);
+    }
+    if let Some(c) = css {
+        marker.add_css_class(c);
+    }
+}
+
+#[allow(dead_code)]
+fn git_marker_for(s: GitStatus) -> (&'static str, Option<&'static str>) {
+    match s {
+        GitStatus::Modified => ("M", Some("limux-fp-git-m")),
+        GitStatus::Added => ("A", Some("limux-fp-git-a")),
+        GitStatus::Deleted => ("D", Some("limux-fp-git-d")),
+        GitStatus::Untracked => ("?", Some("limux-fp-git-u")),
+        GitStatus::Conflict => ("!", Some("limux-fp-git-c")),
+        GitStatus::Ignored | GitStatus::Clean => ("", None),
+    }
+}
+
+#[allow(dead_code)]
+pub fn apply_model_to_store(model: &TreeModel, store: &gtk4::gio::ListStore) {
+    store.remove_all();
+    for row in &model.rows {
+        let obj = RowObject::from_row(row);
+        store.append(&obj);
+    }
 }
